3b0094afd497ff037eb2851160872ed8045be168
Kyle Zhao <kzhao.sw@gmail.com>
2019-08-14 23:12:44 -0400

    DEV: migrate controller spec to request spec

--- spec/requests/feed_settings_controller_spec.rb
+++ spec/requests/feed_settings_controller_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 describe DiscourseWellfed::FeedSettingsController do
-  routes { DiscourseWellfed::Engine.routes }
-
-  let!(:user) { log_in(:admin) }
+  let!(:admin) { Fabricate(:admin) }
 
   before do
+    sign_in(admin)
+
     SiteSetting.wellfed_enabled = true
     SiteSetting.wellfed_feed_setting = [
       ['https://www.example.com/feed', 'system'],
@@ -22,21 +22,28 @@ describe DiscourseWellfed::FeedSettingsController do
         root: :feed_settings,
       ).to_json
 
-      get :show, format: :json
+      get '/admin/plugins/wellfed/feed_settings.json'
 
-      expect(response).to be_success
+      expect(response.status).to eq(200)
       expect(response.body).to eq(expected_json)
     end
   end
 
   describe '#update' do
     it 'updates SiteSetting.wellfed_feed_setting' do
-      put :update, format: :json, params: {
-        feed_settings: [{ feed_url: 'https://www.newsite.com/feed', author_username: 'system' }]
+      put '/admin/plugins/wellfed/feed_settings.json', params: {
+        feed_settings: [
+          {
+            feed_url: 'https://www.newsite.com/feed',
+            author_username: 'system'
+          }
+        ]
       }
 
-      expect(response).to be_success
-      expect(SiteSetting.wellfed_feed_setting).to eq([['https://www.newsite.com/feed', 'system']].to_yaml)
+      expect(response.status).to eq(200)
+      expect(SiteSetting.wellfed_feed_setting).to eq([
+        ['https://www.newsite.com/feed', 'system']
+      ].to_yaml)
     end
   end
 end
