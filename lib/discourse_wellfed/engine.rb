1139f3e181158fb5e00278466ca3fe3b9f78f48b
Kyle Zhao <kzhao.sw@gmail.com>
2019-08-20 22:59:13 -0400

    Drop TopicRetriever#perform_retrieve monkey patch
    `TopicRetriever` is monkey-patched so that a topic could be fetched
    directly from its feed when `/embed/comments` is hit.
    
    However, since feed polling is rate limited, the monkey patched method
    will not ran in most cases and the post will be eventually updated when
    the next time the job runs.
    
    This closes #2.

--- lib/discourse_wellfed/engine.rb
+++ lib/discourse_wellfed/engine.rb
@@ -9,10 +9,6 @@ module DiscourseWellfed
       Dir[File.expand_path(File.join('..', '..', '..', 'app', 'jobs', 'discourse_wellfed', '*.rb'), __FILE__)].each do |job|
         require_dependency job
       end
-
-      Dir[File.expand_path(File.join('..', '..', '..', 'discourse_extensions', '*.rb'), __FILE__)].each do |discourse_extension|
-        require_dependency discourse_extension
-      end
     end
   end
 end
