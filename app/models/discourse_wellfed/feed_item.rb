46b749f0bc139d177480479bf6348337c45dcf50
Kyle Zhao <kzhao.sw@gmail.com>
2019-08-20 22:34:44 -0400

    FIX: Fallback to `summary` tag for content

--- app/models/discourse_wellfed/feed_item.rb
+++ app/models/discourse_wellfed/feed_item.rb
@@ -15,18 +15,26 @@ module DiscourseWellfed
     def content
       content = nil
 
-      %i[content_encoded content description].each do |content_element_name|
-        content ||= @accessor.element_content(content_element_name)
+      CONTENT_ELEMENT_TAG_NAMES.each do |tag_name|
+        break if content = @accessor.element_content(tag_name)
       end
 
       content&.force_encoding('UTF-8')&.scrub
     end
 
     def title
-      @accessor.element_content(:title).force_encoding('UTF-8').scrub
+      @accessor.element_content(:title)&.force_encoding('UTF-8')&.scrub
     end
 
     private
+
+    # The tag name's relative order implies its priority.
+    CONTENT_ELEMENT_TAG_NAMES = %i[
+      content_encoded
+      content
+      description
+      summary
+    ]
 
     def url?(link)
       link.present? && link =~ /^https?\:\/\//
--- spec/models/feed_item_spec.rb
+++ spec/models/feed_item_spec.rb
@@ -4,37 +4,58 @@ require 'rails_helper'
 require 'rss'
 
 RSpec.describe DiscourseWellfed::FeedItem do
-  context 'for ATOM feed' do
-    let(:atom_feed) { RSS::Parser.parse(file_from_fixtures('feed.atom', 'feed'), false) }
-    let(:feed_item) { DiscourseWellfed::FeedItem.new(atom_feed.items.first) }
+  RSpec.shared_examples 'correctly parses the feed' do |**expected|
+    let(:feed_item) { DiscourseWellfed::FeedItem.new(raw_feed_item) }
 
-    describe '#content' do
-      it { expect(feed_item.content).to eq('<p>This is the body &amp; content. </p>') }
-    end
-
-    describe '#url' do
-      it { expect(feed_item.url).to eq('https://blog.discourse.org/2017/09/poll-feed-spec-fixture/') }
-    end
-
-    describe '#title' do
-      it { expect(feed_item.title).to eq('Poll Feed Spec Fixture') }
-    end
+    it { expect(feed_item.content).to eq(expected[:content]) }
+    it { expect(feed_item.url).to eq(expected[:url]) }
+    it { expect(feed_item.title).to eq(expected[:title]) }
   end
 
-  context 'for RSS feed' do
-    let(:rss_feed) { RSS::Parser.parse(file_from_fixtures('feed.rss', 'feed'), false) }
-    let(:feed_item) { DiscourseWellfed::FeedItem.new(rss_feed.items.first) }
+  context 'Empty item' do
+    let(:raw_feed_item) { {} }
+    include_examples(
+      'correctly parses the feed',
+      content: nil,
+      url: nil,
+      title: nil,
+    )
+  end
 
-    describe '#content' do
-      it { expect(feed_item.content).to eq('<p>This is the body &amp; content. </p>') }
-    end
+  context 'RSS item' do
+    let(:feed) { RSS::Parser.parse(file_from_fixtures('feed.rss', 'feed')) }
+    let(:raw_feed_item) { feed.items.first }
 
-    describe '#url' do
-      it { expect(feed_item.url).to eq('https://blog.discourse.org/2017/09/poll-feed-spec-fixture/') }
-    end
+    include_examples(
+      'correctly parses the feed',
+      content: '<p>This is the body &amp; content. </p>',
+      url: 'https://blog.discourse.org/2017/09/poll-feed-spec-fixture/',
+      title: 'Poll Feed Spec Fixture',
+    )
+  end
 
-    describe '#title' do
-      it { expect(feed_item.title).to eq('Poll Feed Spec Fixture') }
-    end
+  context 'ATOM item with content element' do
+    let(:feed) { RSS::Parser.parse(file_from_fixtures('feed.atom', 'feed')) }
+    let(:raw_feed_item) { feed.entries.first }
+
+    include_examples(
+      'correctly parses the feed',
+      content: '<p>This is the body &amp; content. </p>',
+      url: 'https://blog.discourse.org/2017/09/poll-feed-spec-fixture/',
+      title: 'Poll Feed Spec Fixture',
+    )
+  end
+
+  context 'ATOM item with summary element' do
+    let(:raw_feed) { wellfed_file_fixture('no_content_only_summary.atom').read }
+    let(:feed) { RSS::Parser.parse(raw_feed) }
+    let(:raw_feed_item) { feed.entries.first }
+
+    include_examples(
+      'correctly parses the feed',
+      content: 'Here are some random descriptions...',
+      url: 'https://blog.discourse.org/2017/09/poll-feed-spec-fixture/',
+      title: 'Poll Feed Spec Fixture',
+    )
   end
 end
