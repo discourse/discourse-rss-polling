a387c44a9bef7306c88f6cd641c4554db7ad5ccf
Mark VanLandingham <markvanlan@gmail.com>
2019-11-12 09:48:31 -0600

    FIX: Ensure that all feed settings can be removed

--- app/controllers/discourse_rss_polling/feed_settings_controller.rb
+++ app/controllers/discourse_rss_polling/feed_settings_controller.rb
@@ -9,9 +9,13 @@ module DiscourseRssPolling
     end
 
     def update
-      # TODO: validator? separate persister?
-      new_feed_settings = (feed_setting_params.presence || []).map do |feed_setting|
-        feed_setting.values_at(:feed_url, :author_username)
+
+      if params[:feed_settings] == []
+        new_feed_settings = []
+      else
+        new_feed_settings = (feed_setting_params.presence || []).map do |feed_setting|
+          feed_setting.values_at(:feed_url, :author_username)
+        end
       end
 
       SiteSetting.rss_polling_feed_setting = new_feed_settings.to_yaml
