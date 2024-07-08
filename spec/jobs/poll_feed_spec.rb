4e85dd44ab2fe03f2989934474ff251934b40931
Jeff Wong <awole20@gmail.com>
2024-07-08 11:45:31 -0700

    FEATURE: add an option to disable updating tags from polling

--- spec/jobs/poll_feed_spec.rb
+++ spec/jobs/poll_feed_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe Jobs::DiscourseRssPolling::PollFeed do
   SiteSetting.rss_polling_enabled = true
   let(:feed_url) { "https://blog.discourse.org/feed/" }
-  let(:author) { Fabricate(:user) }
+  let(:author) { Fabricate(:user, trust_level: 1) }
   let(:raw_feed) { file_from_fixtures("feed.rss", "feed") }
   let(:job) { Jobs::DiscourseRssPolling::PollFeed.new }
 
@@ -28,6 +28,49 @@ RSpec.describe Jobs::DiscourseRssPolling::PollFeed do
       expect(topic.topic_embed.embed_url).to eq(
         "https://blog.discourse.org/2017/09/poll-feed-spec-fixture",
       )
+    end
+
+    context "with a previous poll on a topic with tags" do
+      let(:tag1) { Fabricate(:tag, name: "test-from-rss") }
+      let(:tag2) { Fabricate(:tag, name: "test-update-from-rss") }
+
+      before do
+        SiteSetting.tagging_enabled = true
+        job.execute(
+          feed_url: feed_url,
+          author_username: author.username,
+          discourse_tags: [tag1.name],
+        )
+        Discourse.redis.del("rss-polling-feed-polled:#{Digest::SHA1.hexdigest(feed_url)}")
+      end
+
+      context "with rss polling set to true" do
+        before { SiteSetting.rss_polling_update_tags = true }
+        it "updates tags by default" do
+          topic = author.topics.last
+          job.execute(
+            feed_url: feed_url,
+            author_username: author.username,
+            discourse_tags: [tag2.name],
+          )
+          topic = author.topics.last.reload
+          expect(topic.tags).to match_array([tag2])
+        end
+      end
+
+      context "with rss polling set to false" do
+        before { SiteSetting.rss_polling_update_tags = false }
+
+        it "does not update tags" do
+          job.execute(
+            feed_url: feed_url,
+            author_username: author.username,
+            discourse_tags: [tag2.name],
+          )
+          topic = author.topics.last
+          expect(topic.tags).to match_array([tag1])
+        end
+      end
     end
 
     it "is rate limited by rss_polling_frequency" do
