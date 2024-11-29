43ffdf17cf2b6e2407be5ab1292b20ed09368222
David Taylor <david@taylorhq.com>
2024-11-29 13:13:38 +0000

    DEV: Convert to native class syntax

--- assets/javascripts/discourse/controllers/admin-plugins-rss-polling.js
+++ assets/javascripts/discourse/controllers/admin-plugins-rss-polling.js
@@ -1,19 +1,21 @@
 import Controller from "@ember/controller";
-import { set } from "@ember/object";
+import { action, set } from "@ember/object";
 import { alias } from "@ember/object/computed";
 import { isBlank } from "@ember/utils";
-import discourseComputed, { observes } from "discourse-common/utils/decorators";
+import { observes } from "@ember-decorators/object";
+import discourseComputed from "discourse-common/utils/decorators";
 import RssPollingFeedSettings from "../../admin/models/rss-polling-feed-settings";
 
-export default Controller.extend({
-  feedSettings: alias("model"),
-  saving: false,
-  valid: false,
+export default class AdminPluginsRssPollingController extends Controller {
+  @alias("model") feedSettings;
+
+  saving = false;
+  valid = false;
 
   @discourseComputed("valid", "saving")
   unsavable(valid, saving) {
     return !valid || saving;
-  },
+  }
 
   // TODO: extract feed setting into its own component && more validation
   @observes("feedSettings.@each.{feed_url,author_username}")
@@ -28,38 +30,40 @@ export default Controller.extend({
     });
 
     this.set("valid", overallValidity);
-  },
+  }
 
-  actions: {
-    create() {
-      this.get("feedSettings").addObject({
-        feed_url: null,
-        author_username: null,
-        discourse_category_id: null,
-        discourse_tags: null,
-        feed_category_filter: null,
+  @action
+  create() {
+    this.get("feedSettings").addObject({
+      feed_url: null,
+      author_username: null,
+      discourse_category_id: null,
+      discourse_tags: null,
+      feed_category_filter: null,
+    });
+  }
+
+  @action
+  destroyFeedSetting(feedSetting) {
+    this.get("feedSettings").removeObject(feedSetting);
+    this.send("update");
+  }
+
+  @action
+  update() {
+    this.set("saving", true);
+
+    RssPollingFeedSettings.update(this.get("feedSettings"))
+      .then((updatedSettings) => {
+        this.set("feedSettings", updatedSettings["feed_settings"]);
+      })
+      .finally(() => {
+        this.set("saving", false);
       });
-    },
+  }
 
-    destroy(feedSetting) {
-      this.get("feedSettings").removeObject(feedSetting);
-      this.send("update");
-    },
-
-    update() {
-      this.set("saving", true);
-
-      RssPollingFeedSettings.update(this.get("feedSettings"))
-        .then((updatedSettings) => {
-          this.set("feedSettings", updatedSettings["feed_settings"]);
-        })
-        .finally(() => {
-          this.set("saving", false);
-        });
-    },
-
-    updateAuthorUsername(setting, selected) {
-      set(setting, "author_username", selected.firstObject);
-    },
-  },
-});
+  @action
+  updateAuthorUsername(setting, selected) {
+    set(setting, "author_username", selected.firstObject);
+  }
+}
--- assets/javascripts/discourse/routes/admin-plugins-rss-polling.js
+++ assets/javascripts/discourse/routes/admin-plugins-rss-polling.js
@@ -1,14 +1,14 @@
 import DiscourseRoute from "discourse/routes/discourse";
 import RssPollingFeedSettings from "../../admin/models/rss-polling-feed-settings";
 
-export default DiscourseRoute.extend({
+export default class AdminPluginsRssPolling extends DiscourseRoute {
   model() {
     return RssPollingFeedSettings.show().then((result) => result.feed_settings);
-  },
+  }
 
   setupController(controller, model) {
     controller.setProperties({
       model,
     });
-  },
-});
+  }
+}
